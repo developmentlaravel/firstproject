58ebf4be41986b129f9abd23865d14e0817d5eae
mary m <naderi9310@gmail.com>
2020-12-17 14:22:06 +0330

    media

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,9 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Routing\Route;
 
-class Article extends Model
+class Article extends Model 
 {
     use HasFactory;
+
 
     protected $fillable=[];
 
